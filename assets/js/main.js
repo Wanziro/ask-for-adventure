9a5df1bb215b2a21f1f4c8d1e8f9dab3343cdbad
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-08-22 16:12:04 +0200

    sliders fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
 $("#multiSlider").carousel({
   //   interval: 5000,
 });
+
+// $("#recipeCarousel").carousel({
+//   interval: 10000,
+// });
+
+$(".multi-caraousel .carousel-item").each(function () {
+  var minPerSlide = 4;
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+
+    next.children(":first-child").clone().appendTo($(this));
+  }
+});
